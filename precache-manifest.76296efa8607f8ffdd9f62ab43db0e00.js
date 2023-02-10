d0ff09e92c6646d33a540660dc4dbda8ff50f046
kulkamalsingh <kulkamalsingh377@gmail.com>
2023-02-10 11:36:09 +0530

    Updates

---
+++ precache-manifest.76296efa8607f8ffdd9f62ab43db0e00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "020defb999d8f7d0b9af",
+    "url": "/static/css/main.c74a4989.chunk.css"
+  },
+  {
+    "revision": "020defb999d8f7d0b9af",
+    "url": "/static/js/main.020defb9.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "b56db25ab1c8e8106f9d",
+    "url": "/static/js/2.b56db25a.chunk.js"
+  },
+  {
+    "revision": "1f46550c42e737540ce6",
+    "url": "/static/js/runtime~main.1f46550c.js"
+  },
+  {
+    "revision": "9503809cc73ca7cf014baa2595a85dff",
+    "url": "/static/media/Vector.9503809c.png"
+  },
+  {
+    "revision": "ebcb9f81a849fe0ba7008213a0118a9b",
+    "url": "/index.html"
+  }
+];
